c2e6cb26d3d73d5c7eca98560e31720bb4db509c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-04 16:49:08 -0700

    code

---
+++ skiprunes.go
@@ -0,0 +1,45 @@
+package skip
+
+import (
+	"io"
+
+	"sourcecode.social/reiver/go-erorr"
+)
+
+// SkipRunes skips an leading runes that are in the cutset.
+func SkipRunes(runescanner io.RuneScanner, cutset ...rune) error {
+	if nil == runescanner {
+		return errNilRuneScanner
+	}
+
+	if len(cutset) <= 0 {
+		return nil
+	}
+
+	loop: for {
+		r, size, err := runescanner.ReadRune()
+		switch {
+		case 0 < size:
+			for _, cut := range cutset {
+				if cut == r {
+					continue loop
+				}
+			}
+		case io.EOF == err:
+			return io.EOF
+		case nil != err:
+			return erorr.Errorf("skip: problem reading rune: %w", err)
+		default:
+			return errNoErrorNoRune
+		}
+
+		{
+			err := runescanner.UnreadRune()
+			if nil != err {
+				return err
+			}
+
+			return nil
+		}
+	}
+}
---
+++ skiprunes_test.go
@@ -0,0 +1,116 @@
+package skip_test
+
+import (
+	"testing"
+
+	"io"
+	"strings"
+
+	"sourcecode.social/reiver/go-utf8"
+
+	"github.com/reiver/go-skip"
+)
+
+func TestSkipRune(t *testing.T) {
+
+	tests := []struct{
+		CutSet []rune
+		RuneScanner io.RuneScanner
+		Expected string
+	}{
+		{
+			CutSet: []rune{'\t', ' '},
+			RuneScanner: utf8.NewRuneScanner(strings.NewReader("Hello world! 🙂 wow.")),
+			Expected:                                          "Hello world! 🙂 wow.",
+		},
+
+		{
+			CutSet: []rune{'\t', ' '},
+			RuneScanner: utf8.NewRuneScanner(strings.NewReader(" Hello world! 🙂 wow.")),
+			Expected:                         "Hello world! 🙂 wow.",
+		},
+		{
+			CutSet: []rune{'\t', ' '},
+			RuneScanner: utf8.NewRuneScanner(strings.NewReader("  Hello world! 🙂 wow.")),
+			Expected:                          "Hello world! 🙂 wow.",
+		},
+		{
+			CutSet: []rune{'\t', ' '},
+			RuneScanner: utf8.NewRuneScanner(strings.NewReader("   Hello world! 🙂 wow.")),
+			Expected:                           "Hello world! 🙂 wow.",
+		},
+		{
+			CutSet: []rune{'\t', ' '},
+			RuneScanner: utf8.NewRuneScanner(strings.NewReader("    Hello world! 🙂 wow.")),
+			Expected:                            "Hello world! 🙂 wow.",
+		},
+
+		{
+			CutSet: []rune{'\t', ' '},
+			RuneScanner: utf8.NewRuneScanner(strings.NewReader("\tHello world! 🙂 wow.")),
+			Expected:                          "Hello world! 🙂 wow.",
+		},
+		{
+			CutSet: []rune{'\t', ' '},
+			RuneScanner: utf8.NewRuneScanner(strings.NewReader("\t\tHello world! 🙂 wow.")),
+			Expected:                            "Hello world! 🙂 wow.",
+		},
+		{
+			CutSet: []rune{'\t', ' '},
+			RuneScanner: utf8.NewRuneScanner(strings.NewReader("\t\t\tHello world! 🙂 wow.")),
+			Expected:                              "Hello world! 🙂 wow.",
+		},
+		{
+			CutSet: []rune{'\t', ' '},
+			RuneScanner: utf8.NewRuneScanner(strings.NewReader("\t\t\t\tHello world! 🙂 wow.")),
+			Expected:                                "Hello world! 🙂 wow.",
+		},
+
+		{
+			CutSet: []rune{'\t', ' '},
+			RuneScanner: utf8.NewRuneScanner(strings.NewReader(" \t\t   \t\t\t\t Hello world! 🙂 wow.")),
+			Expected:                                         "Hello world! 🙂 wow.",
+		},
+	}
+
+	testloop: for testNumber, test := range tests {
+
+		var runescanner io.RuneScanner = test.RuneScanner
+
+		err := skip.SkipRunes(runescanner, test.CutSet...)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error but actually got one." , testNumber)
+			t.Logf("ERROR: (%T) %s", err, err)
+			continue testloop
+		}
+
+		{
+			var buffer strings.Builder
+
+			miniloop: for {
+				r, size, err := runescanner.ReadRune()
+				if 0 < size {
+					buffer.WriteRune(r)
+				}
+				if io.EOF == err {
+					break miniloop
+				}
+				if nil != err {
+					t.Errorf("For test #%d, did not expect an error but actually got one." , testNumber)
+					t.Logf("ERROR: (%T) %s", err, err)
+					continue testloop
+				}
+			}
+
+			expected := test.Expected
+			actual   := buffer.String()
+
+			if expected != actual {
+				t.Errorf("For test #%d, the actual (after skipping) 'content' is not what was expected.", testNumber)
+				t.Logf("EXPECTED: %q", expected)
+				t.Logf("ACTUAL:   %q", actual)
+				continue testloop
+			}
+		}
+	}
+}
