af61311f10f89a0b8413f3cb537acadc18e5fef8
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-23 10:50:04 -0700

    skip.ErrRuneNotFound

--- errors.go
+++ errors.go
@@ -8,3 +8,7 @@ const (
 	errNilRuneScanner = erorr.Error("skip: nil rune scanner")
 	errNoErrorNoRune  = erorr.Error("skip: problem reading rune: did not receive an error but also did not receive a rune")
 )
+
+const (
+	ErrRuneNotFound = erorr.Error("rune not found")
+)
