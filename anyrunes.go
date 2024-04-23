30bfa559e10988def6258d886d4900de6eb1e07b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-23 10:51:47 -0700

    made skip.AnyRunes() use skip.OneOrMoreRunes()

--- anyrunes.go
+++ anyrunes.go
@@ -2,44 +2,14 @@ package skip
 
 import (
 	"io"
-
-	"sourcecode.social/reiver/go-erorr"
 )
 
 // AnyRunes skips zero, one, or more leading runes that are in the cutset.
 func AnyRunes(runescanner io.RuneScanner, cutset ...rune) error {
-	if nil == runescanner {
-		return errNilRuneScanner
-	}
-
-	if len(cutset) <= 0 {
+	err := OneOrMoreRunes(runescanner, cutset...)
+	if ErrRuneNotFound == err {
 		return nil
 	}
 
-	loop: for {
-		r, size, err := runescanner.ReadRune()
-		switch {
-		case 0 < size:
-			for _, cut := range cutset {
-				if cut == r {
-					continue loop
-				}
-			}
-		case io.EOF == err:
-			return io.EOF
-		case nil != err:
-			return erorr.Errorf("skip: problem reading rune: %w", err)
-		default:
-			return errNoErrorNoRune
-		}
-
-		{
-			err := runescanner.UnreadRune()
-			if nil != err {
-				return err
-			}
-
-			return nil
-		}
-	}
+	return err
 }
